9ed22b27b9ef9d0d2ac3e03cbc44ffec34482078
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-07-05 19:35:35 +0530

    Improve documentation

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,4 @@
 # Improve documentation at 2024-10-17 09:11:48 - 28131
 # Update dependencies at 2024-10-21 15:28:53 - 9410
 # Adjust test configuration at 2024-10-25 09:49:43 - 3505
+# Improve documentation at 2024-11-02 16:24:39 - 19094
