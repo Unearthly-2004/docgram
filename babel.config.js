384e4ad4136f6a0977f69081b10890c3dea0aed8
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-07-11 00:31:14 +0530

    Update nodemon config

--- babel.config.js
+++ babel.config.js
@@ -3,3 +3,4 @@
 # Update Docker setup at 2024-10-03 13:51:40 - 31132
 # Refine prepublish script at 2024-10-17 14:55:57 - 20897
 # Update dependencies at 2024-10-22 11:21:05 - 32057
+# Update nodemon config at 2024-10-25 12:12:44 - 2936
