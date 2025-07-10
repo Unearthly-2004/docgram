d565b3bbcb88ebf39a93eb66adb0fde17c904d08
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-07-11 00:31:05 +0530

    Update nodemon config

--- jest.config.js
+++ jest.config.js
@@ -6,3 +6,4 @@
 # Improve documentation at 2024-11-07 09:19:24 - 14712
 # Refine prepublish script at 2024-10-02 14:00:44 - 26262
 # Update entry-point at 2024-10-05 09:51:38 - 20709
+# Update nodemon config at 2024-10-05 13:03:51 - 9498
