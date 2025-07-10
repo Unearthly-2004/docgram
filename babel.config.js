e086cc44b79adae7c7d77192b0da2b60ec380309
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-07-11 00:31:10 +0530

    Refine prepublish script

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
 # Minor fix at 2024-10-10 10:20:49 - 21577
 # Update wrangler config at 2024-10-31 09:35:45 - 31268
 # Update Docker setup at 2024-10-03 13:51:40 - 31132
+# Refine prepublish script at 2024-10-17 14:55:57 - 20897
