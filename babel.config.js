d05f7d9c34f72f9128816e10008373f3ee949616
Shrujan Kharwadey <kharwadeyshrujan@gmail.com>
2025-07-11 00:31:11 +0530

    Update dependencies

--- babel.config.js
+++ babel.config.js
@@ -2,3 +2,4 @@
 # Update wrangler config at 2024-10-31 09:35:45 - 31268
 # Update Docker setup at 2024-10-03 13:51:40 - 31132
 # Refine prepublish script at 2024-10-17 14:55:57 - 20897
+# Update dependencies at 2024-10-22 11:21:05 - 32057
